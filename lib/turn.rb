103dc843445928b25390a9caf1278e2921a8ebf5
Isaac Mugenyi <ikaliisa@gmail.com>
2022-03-25 15:14:11 +0000

    first turn instruction

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,9 @@ class Turn
   end
 
   def take_turn
+    puts "reading current board"
+    puts "current board read establishing what to do"
+    puts "iterate over current board"
     self.turn_number += 1
   end
 end
