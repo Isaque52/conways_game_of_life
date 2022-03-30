ca9ff975f2fd082369e750c75b678832e06b2740
Isaac Mugenyi <ikaliisa@gmail.com>
2022-03-30 14:14:30 +0100

    turn interates with board

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,10 +5,15 @@ class Turn
     @turn_number = 0
   end
 
-  def take_turn
+  def take_turn(current_board)
     puts "reading current board"
-    puts "current board read establishing what to do"
+    p current_board
     puts "iterate over current board"
+    current_board.cells.each do |row|
+      row.each do |cell|
+        puts "this cell value is #{cell}"
+      end
+    end
     self.turn_number += 1
   end
 end
