3961a0ebdda6d3dce8b69e09da0377904672d899
Isaac Mugenyi <ikaliisa@gmail.com>
2022-03-30 15:15:14 +0100

    initial neighbourslogic done

--- lib/board.rb
+++ lib/board.rb
@@ -4,4 +4,37 @@ class Board
   def initialize
     @cells = Array.new(3) { Array.new(3) { :dead } }
   end
+
+  def status(x, y)
+    cells[y][x]
+  end
+
+  def neighbours(x, y)
+    top_neighbours(x, y) + middle_neighbours(x, y) + bottom_neighbours(x, y)
+  end
+
+  private
+  
+  def top_neighbours(x, y)
+    [
+      [x - 1, y + 1],
+      [x, y + 1],
+      [x + 1, y + 1]
+    ]
+  end
+
+  def middle_neighbours(x, y)
+    [
+      [x - 1, y],
+      [x + 1, y]
+    ]
+  end
+
+  def bottom_neighbours(x, y)
+    [
+      [x - 1, y - 1],
+      [x, y - 1],
+      [x + 1, y - 1]
+    ]
+  end
 end
--- lib/rules.rb
+++ lib/rules.rb
@@ -1,7 +1,9 @@
 class Rules
   attr_accessor :current_board
-  
+
   def initialize(current_board)
     @current_board = current_board
   end
+
+
 end
