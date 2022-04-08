1f3dd1f94302f1abdfb5d264d963c385a5982231
Isaac Mugenyi <ikaliisa@gmail.com>
2022-04-08 14:50:32 +0100

    three quaters of the game done

--- lib/all.rb
+++ lib/all.rb
@@ -1,4 +1,3 @@
 require_relative 'board'
 require_relative 'rules'
-require_relative 'turn'
 require_relative 'game'
--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,28 @@
 class Game
-  attr_reader :previous_board, :rules, :turn
+  attr_reader :current_board
+  attr_accessor :turn
 
   def initialize
-    @previous_board = Board.new
-    @rules = Rules.new(@previous_board)
-    @turn = Turn.new
+    @current_board = Board.new
+    @turn = 0
+  end
+
+  def rules
+    Rules.new(current_board)
+  end
+
+  def take_turn
+    puts "reading current board"
+    p current_board
+    puts "iterate over current board"
+    current_board.cells.each_with_index do |row, y|
+      row.each_with_index do |cell, x|
+        puts "this cell value is #{cell}-coords#{x} #{y}"
+        puts "this cell-survival rule #{rules.survive?(x, y)}"
+      end
+    end
+    self.turn += 1
+    self.current_board = :TBD
   end
 end
+
